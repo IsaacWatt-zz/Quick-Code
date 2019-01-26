d48a9449695924a14837c082876307836a8a14ab
Isaac Watt <isaacwatt@icloud.com>
2019-01-26 18:32:47 -0500

    codemirror init

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,6 +3,20 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Particles from 'react-particles-js';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/styles/hljs';
+import TextField from '@material-ui/core/TextField';
+require([
+  "cm/lib/codemirror", "cm/mode/htmlmixed/htmlmixed"
+], function(CodeMirror) {
+  CodeMirror.fromTextArea(document.getElementById("code"), {
+    lineNumbers: true,
+    mode: "htmlmixed"
+  });
+});
+
+import './text.css'
 
 const styles = theme => ({
   root: {
@@ -12,7 +26,7 @@ const styles = theme => ({
     padding: theme.spacing.unit * 2,
     textAlign: 'center',
     color: theme.palette.text.secondary,
-  },
+  }
 });
 
 function FullWidthGrid(props) {
@@ -20,7 +34,11 @@ function FullWidthGrid(props) {
 
   return (
     <div className={classes.root}>
-
+      <Grid container spacing={24}>
+        <Grid item xs={12} sm={12}>
+          <textarea id="myTextarea" type="text"></textarea>
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ src/components/textIDE.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+  const styles = {
+    root: {
+      flexGrow: 1,
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+    app: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        color: 'white',
+
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)'
+    }
+  };
+
+  function NavBar(props) {
+    const { classes } = props;
+    return (
+      <div className={classes.root}>
+        <AppBar className={classes.app} position="static">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Quick Code
+            </Typography>
+            <Button color="inherit">Speak Code</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+
+  NavBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+
+  export default withStyles(styles)(NavBar);
