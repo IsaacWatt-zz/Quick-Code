e347f89026ce9d704bc0201dd07770fd93fc5ad9
Isaac Watt <isaacwatt@icloud.com>
2019-01-26 17:03:17 -0500

    initial boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NavBar from './components/NavBar'
+import CenteredGrid from './components/CharacterCard'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <CenteredGrid />
+      </div>
+    )
+  }
+}
+export default App
