096c39454f3f5e29dbd4c5d5d2c6d4a56ceefe53
IOkal <iyadokal@gmail.com>
2019-01-27 01:13:22 -0800

    Added Voice Recorder and waveform UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react'
-import NavBar from './components/NavBar'
-import CenteredGrid from './components/CharacterCard'
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import CenteredGrid from "./components/CharacterCard";
+import Code from "./components/Code";
+
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
         <CenteredGrid />
+        <Code />
       </div>
-    )
+    );
   }
 }
-export default App
+
+export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,50 +1,55 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
 
-  const styles = {
-    root: {
-      flexGrow: 1,
-    },
-    grow: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginLeft: -12,
-      marginRight: 20,
-    },
-    app: {
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        color: 'white',
+let stream;
 
-        padding: '0 30px',
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)'
-    }
-  };
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  app: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    color: "white",
 
-  function NavBar(props) {
-    const { classes } = props;
-    return (
-      <div className={classes.root}>
-        <AppBar className={classes.app} position="static">
-          <Toolbar>
-            <Typography variant="h6" color="inherit" className={classes.grow}>
-              Quick Code
-            </Typography>
-            <Button color="inherit">Speak Code</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
+    padding: "0 30px",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)"
   }
+};
 
-  NavBar.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
+function NavBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.app} position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            Quick Code
+          </Typography>
+          <Button color="inherit">Speak Code</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+//}
 
-  export default withStyles(styles)(NavBar);
+//function NavBar(props) {}
+
+NavBar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(NavBar);
